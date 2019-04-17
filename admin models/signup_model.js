0bce195e62bb2e23ef295d6da81d1e7a1c82301f
sonu27899 <shahsmit929@gmail.com>
2019-04-17 20:57:29 +0530

    LAST UPDATE

--- admin models/signup_model.js
+++ admin models/signup_model.js
@@ -5,7 +5,7 @@ var signup={
         return db.query("select * from user_tbl",callback); 
         },
     addUserinformation:function(item,callback){
-        return db.query("insert into user_tbl values(?,?,?,?,?,?,?,?)",[item.user_email,item.user_password,item.user_name,item.user_mobileno,item.user_city,item.user_gender,item.user_address,null],callback);
+        return db.query("insert into user_tbl values(?,?,?,?,?,?,?)",[item.user_email,item.user_password,item.user_name,item.user_mobileno,item.user_city,item.user_gender,item.user_address],callback);
      },
      getUserByType:function(user_type,callback){
         return db.query("select * from user_tbl where user_type=?",[user_type],callback); 
